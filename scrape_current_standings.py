e8df9e8d884ca14c5748d6b181d58e8482281965
Michael Warkentin <mwarkentin@gmail.com>
2014-03-19 09:26:22 -0400

    WIP: Scrape current standings

---
+++ scrape_current_standings.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import json
+from pprint import pprint
+
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('http://api.thescore.com/nhl/standings')
+    standings = r.json()
+
+    teams = []
+
+    for team in standings:
+        name = team['team']['abbreviation']
+        logo_tiny = team['team']['logos']['tiny']
+        conference = team['team']['conference']
+        points = team['points']
+        games_remaining = 82 - team['games_played']
+        potential_points = points + (games_remaining * 2)
+
+        team_dict = {
+            'name': name,
+            'points': points,
+            'potential_points': potential_points,
+            'logo_tiny': logo_tiny,
+            'conference': conference,
+        }
+
+        teams.append(team_dict)
+
+    pprint(teams)
+
+    json_dict = {
+        'teams': teams,
+    }
+
+    with open('data.json', 'wb') as fp:
+        json.dump(json_dict, fp)
